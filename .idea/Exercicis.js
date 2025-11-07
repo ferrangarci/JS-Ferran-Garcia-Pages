2075d8671683f44944f444f9e731ba21d1370066
Ferran <ferran.garcia@institutvidreres.cat>
2025-11-07 10:59:44 +0100

    Exercici 3

--- .idea/Exercicis.js
+++ .idea/Exercicis.js
@@ -10,7 +10,7 @@ console.error('11/12/2006');
 
 // Exercici 2
 
-
+/*
 var a = 'hola'
 let b = false
 const c = Number.MAX_VALUE
@@ -18,3 +18,12 @@ const c = Number.MAX_VALUE
 console.log(a)
 console.log(b)
 console.log(c)
+ */
+
+// Exercici 3
+
+var a = 'hola' // canvia el format en el que s'ensenya i el color
+let b = false
+
+console.log(a)
+console.log(b)
