dff14aa5e5512c842c113be91e021f840b0712c5
Ferran <ferran.garcia@institutvidreres.cat>
2025-11-07 10:56:21 +0100

    Exercici 2

--- .idea/Exercicis.js
+++ .idea/Exercicis.js
@@ -1,6 +1,20 @@
 
 // Exercici 1
 
+/*
 console.log('Hola');
 console.warn('Soc en Ferran');
 console.error('11/12/2006');
+
+ */
+
+// Exercici 2
+
+
+var a = 'hola'
+let b = false
+const c = Number.MAX_VALUE
+
+console.log(a)
+console.log(b)
+console.log(c)
