bb0e494d8d8bdfbe7ad9c0c73327088cce106840
Ferran <ferran.garcia@institutvidreres.cat>
2025-11-07 11:28:11 +0100

    Exercici 3 nou

--- .idea/Exercicis.js
+++ .idea/Exercicis.js
@@ -22,13 +22,10 @@ console.log(c)
 
 // Exercici 3
 
-/*
-var a = 'hola' // canvia el format en el que s'ensenya i el color
-let b = false
-
-console.log(a)
 console.log(b)
- */
+var a = 'hola' //surten coses diferents
+let b = 'hola'
+
 
 // Exercici 4
 
@@ -41,7 +38,9 @@ console.log(a) // SI DEIXA, PERQUE LES VARIABLES ACCEPTEN CANVIS
 
 // Exercici 5
 
+/*
 const a = 'hola'
 console.log(a)
 a = Number.MAX_VALUE
 console.log(a) // NO DEIXA, PERQUE LES CONSTANTS NO ACCEPTEN CANVIS
+ */
