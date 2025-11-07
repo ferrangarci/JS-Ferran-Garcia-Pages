6e104e0cb58cf9b77ee409d134eb422193db0916
Ferran <ferran.garcia@institutvidreres.cat>
2025-11-07 11:13:46 +0100

    Exercici 5

--- .idea/Exercicis.js
+++ .idea/Exercicis.js
@@ -36,8 +36,13 @@ console.log(b)
 var a = 'hola'
 console.log(a)
 a = Number.MAX_VALUE
-console.log(a) // SI DEIXA, PERQUE LES VARIABLES ACCEPTE
+console.log(a) // SI DEIXA, PERQUE LES VARIABLES ACCEPTEN CANVIS
  */
 
 // Exercici 5
 
+
+const a = 'hola'
+console.log(a)
+a = Number.MAX_VALUE
+console.log(a) // NO DEIXA, PERQUE LES CONSTANTS NO ACCEPTEN CANVIS
